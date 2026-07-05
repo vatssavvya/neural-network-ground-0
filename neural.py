436498f46962b039619cdc956ff9fb703fa78aba
Savya Vats <68233028+vatssavyya@users.noreply.github.com>
2026-07-05 17:11:47 -0400

    output modified

--- neural.py
+++ neural.py
@@ -9,7 +9,9 @@ bias2 = 3
 bias3 = 0.2
 
 
-output = inputs[0]*weights[0] + inputs[1]*weights[1] 
-+ inputs[2]*weights[2] + inputs[3]*weights[3] + bias
+output = [inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + inputs[3]*weights[3] + bias,
+          inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias2,
+          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias3
+]
 
 print(output)
