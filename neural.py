4e6ab6b63dbd937dd5b808905e8d1db25a71e04e
Savya Vats <68233028+vatssavyya@users.noreply.github.com>
2026-07-05 17:07:59 -0400

    added more inputs

--- neural.py
+++ neural.py
@@ -1,7 +1,15 @@
-inputs = [3.4, 1.3, 2.4]
-weights = [2.3, 4.3, 0.5]
+inputs = [3.4, 1.3, 2.4, 3]
+
+weights = [2.3, 4.3, 0.5, 1]
+weights1 = [2.3, 4.3, 0.5, 1]
+weights2 = [2.3, 4.3, 0.5, 1]
+
 bias = 3
+bias2 = 3
+bias3 = 3
 
 
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+output = inputs[0]*weights[0] + inputs[1]*weights[1] 
++ inputs[2]*weights[2] + inputs[3]*weights[3] + bias
+
 print(output)
