e87f6857efe24a1b2ec6a7d2b8914aac39339635
Savya Vats <savyavats@gmail.com>
2026-07-06 19:38:34 -0400

    converted neuron calculation into a cleaner loop

--- neural.py
+++ neural.py
@@ -6,6 +6,14 @@ weights1 = [[0.5, 0.41, 0.53, 0.1],
 
 bias1 = [2, 3, 0.2]
 
-bias = 2
-bias2 = 3
-bias3 = 0.2
+layerOutputs = []
+
+for neuron_weights, neuron_bias in zip(weights1, bias1):
+    neuronOutput = 0
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuronOutput += n_input * weight
+    neuronOutput += neuron_bias
+    layerOutputs.append(neuronOutput)
+
+print(layerOutputs)
+
