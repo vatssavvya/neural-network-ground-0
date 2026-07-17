f44733af3ebb0b6662acbeaa550a893e05f7c7d3
Savya Vats <savyavats@gmail.com>
2026-07-17 16:52:09 -0400

    commenting

--- neural.py
+++ neural.py
@@ -2,9 +2,9 @@ import numpy as np
 
 np.random.seed(0)
 
-X = [[3.4, 1.3, 2.4, 3],
+X = [[3.4, 1.3, 2.4, 3], #random batch of inputs, modify
      [1.3, 3.1, 5.3, 0.5],
-     [1.4, 5.3, 3.3, -0.8]]
+     [1.4, 5.3, 3.3, -0.8]] 
 
 #modify so that each neuron has its own activation function
 #start with ReLU, then progress to implementing softmax
