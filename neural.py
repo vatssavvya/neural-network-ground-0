b9440ae6d7ed2f69d2ef3881aa5b6af1e783eae2
Savya Vats <savyavats@gmail.com>
2026-07-17 16:51:09 -0400

    remove old scrapped code

--- neural.py
+++ neural.py
@@ -23,39 +23,3 @@ print(layer1.output)
 layer2.forward(layer1.output)
 print(layer2.output)
 
-
-
-
-""" 
-weights1 = [[0.5, 0.41, 0.53, 0.1], 
-            [.23, 0.4, 0.5, 1],
-            [.25, .43, 0.5, 0.42]]
-
-bias1 = [2, 3, 0.2]
-
-weights2 = [[0.4, 0.44, 0.52], 
-            [.24, 0.21, 0.3,],
-            [.26, .63, 0.15]]
-
-bias2 = [1.2, 2.4, 0.5]
-
-layer1_outputs = np.dot(inputs, np.array(weights1).T) + bias1
-
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + bias2
-
-print(layer2_outputs)
-"""
-"""
-layerOutputs = []
-
-
-
-for neuron_weights, neuron_bias in zip(weights1, bias1):
-    neuronOutput = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuronOutput += n_input * weight
-    neuronOutput += neuron_bias
-    layerOutputs.append(neuronOutput)
-
-print(layerOutputs)
-"""
