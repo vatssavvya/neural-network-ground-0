2ddc5d2d1ab4c9b242f9e81e9ad0742a0e654866
Savya Vats <savyavats@gmail.com>
2026-07-18 12:37:39 -0400

    clean up code

--- neural.py
+++ neural.py
@@ -7,7 +7,7 @@ X = [[3.4, 1.3, 2.4, 3],
      [1.4, 5.3, 3.3, -0.8]] 
 
 #modify so that each neuron has its own activation function
-#start with ReLU, then progress to implementing softmax
+
 class layer_dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = np.random.randn(n_inputs, n_neurons)
